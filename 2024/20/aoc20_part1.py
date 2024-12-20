3fc31f2e8ab403ab9ea126c574a926bd4b47bb1a
Petr Zemek <s3rvac@petrzemek.net>
2024-12-20 16:19:50 +0100

    aoc-2024-20: Add solution to part 1

---
+++ 2024/20/aoc20_part1.py
@@ -0,0 +1,146 @@
+#
+# Advent of Code 2024, day 20, part 1
+#
+
+import collections
+import itertools
+import textwrap
+import unittest
+
+
+NEIGHBOR_DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+TILE_START = "S"
+TILE_END = "E"
+TILE_WALL = "#"
+
+
+def read_input():
+    with open("input", encoding="utf-8") as f:
+        return f.read()
+
+
+def parse_input(input):
+    return [list(line) for line in input.strip().split("\n")]
+
+
+def all_non_wall_tiles(map):
+    for i in range(1, len(map) - 1):
+        for j in range(1, len(map[i]) - 1):
+            if map[i][j] != TILE_WALL:
+                yield i, j
+
+
+def find_tile(map, tile):
+    for i, j in all_non_wall_tiles(map):
+        if map[i][j] == tile:
+            return i, j
+    raise AssertionError(f"tile {tile} not found")
+
+
+def taxicab_distance(i1, j1, i2, j2):
+    # https://en.wikipedia.org/wiki/Taxicab_geometry
+    return abs(i1 - i2) + abs(j1 - j2)
+
+
+def all_pairs_of_free_space_at_most_length_apart(map, max_length):
+    pairs = []
+    for (i1, j1), (i2, j2) in itertools.product(all_non_wall_tiles(map), repeat=2):
+        if taxicab_distance(i1, j1, i2, j2) <= max_length:
+            pairs.append(((i1, j1), (i2, j2)))
+    return pairs
+
+
+def get_times_to_get_to_each_tile_from_start_tile(map, start_tile):
+    # Use breadth-first search from the start tile to get a mapping of each
+    # tile to the time required to get to it from the start tile.
+    times_map = {}
+    visited = set()
+    # Format: (i, j, time)
+    q = collections.deque([(*start_tile, 0)])
+
+    while q:
+        i, j, time = q.popleft()
+        times_map[(i, j)] = time
+        visited.add((i, j))
+
+        for di, dj in NEIGHBOR_DIRECTIONS:
+            new_i, new_j = i + di, j + dj
+            if (
+                0 <= new_i < len(map)
+                and 0 <= new_j < len(map[i])
+                and map[new_i][new_j] != TILE_WALL
+                and (new_i, new_j) not in visited
+            ):
+                q.append((new_i, new_j, time + 1))
+
+    return times_map
+
+
+def get_cheat_count_to_finish_race_and_save_time(map, cheat_length, min_time_save):
+    # We do it this way:
+    #
+    # 1) Get a mapping of each tile to the time required to get to it from the
+    #    START tile.
+    # 2) Get a mapping of each tile to the time required to get to it from the
+    #    END tile.
+    # 3) Generate all possible pairs of free space that are at most
+    #    `cheat_length` apart. These are our only options for cheating.
+    # 4) For all those pairs `(m, n)`, check if we can save enough time by
+    #    going from the START tile to `m`, then from `m to `n`, and then from
+    #    `n` to the END tile.
+    start_tile = find_tile(map, TILE_START)
+    times_start = get_times_to_get_to_each_tile_from_start_tile(map, start_tile)
+
+    end_tile = find_tile(map, TILE_END)
+    times_end = get_times_to_get_to_each_tile_from_start_tile(map, end_tile)
+
+    baseline = times_start[end_tile]
+
+    cheat_count = 0
+    pairs = all_pairs_of_free_space_at_most_length_apart(map, cheat_length)
+    for (i1, j1), (i2, j2) in pairs:
+        time = (
+            times_start[(i1, j1)]
+            + taxicab_distance(i1, j1, i2, j2)
+            + times_end[(i2, j2)]
+        )
+        if baseline - time >= min_time_save:
+            cheat_count += 1
+    return cheat_count
+
+
+def run_program(input, cheat_length=2, min_time_save=100):
+    map = parse_input(input)
+    return get_cheat_count_to_finish_race_and_save_time(
+        map, cheat_length, min_time_save
+    )
+
+
+if __name__ == "__main__":
+    result = run_program(read_input())
+    print(result)
+
+
+class Tests(unittest.TestCase):
+    def test_program_returns_correct_result_for_example_input(self):
+        input = textwrap.dedent(
+            """
+            ###############
+            #...#...#.....#
+            #.#.#.#.#.###.#
+            #S#...#.#.#...#
+            #######.#.#.###
+            #######.#.#...#
+            #######.#.###.#
+            ###..E#...#...#
+            ###.#######.###
+            #...###...#...#
+            #.#####.#.###.#
+            #.#...#.#.#...#
+            #.#.#.#.#.#.###
+            #...#...#...###
+            ###############
+            """
+        )
+
+        self.assertEqual(run_program(input, min_time_save=11), 8)
