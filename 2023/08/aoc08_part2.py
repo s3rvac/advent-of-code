045cdc1467de3f0ab515df3eeb4f9010306c6154
Petr Zemek <s3rvac@petrzemek.net>
2024-12-30 16:04:20 +0100

    aoc-2023-08: Slightly improve the solution to part 2
    The runtime has been re-evaluated and rounded off.

--- 2023/08/aoc08_part2.py
+++ 2023/08/aoc08_part2.py
@@ -21,6 +21,7 @@ def parse_input(input):
     network = {}
     for line in lines[2:]:
         m = re.fullmatch(r"(.+) = \((.+), (.+)\)", line)
+        assert m is not None
         current_node, left_node, right_node = m.groups()
         network[current_node] = {"L": left_node, "R": right_node}
 
@@ -28,19 +29,17 @@ def parse_input(input):
 
 
 def count_steps_to_reach_terminal_nodes(instructions, network):
-    # Gist: For each node ending with A, compute the number of steps that it
-    # takes to arrive at a node ending with Z. Then, compute the least common
-    # multiple of those steps, which gives us the total number of steps after
-    # which we arrive at all Z nodes at the same time.
+    # For each node ending with A, compute the number of steps that it takes to
+    # arrive at a node ending with Z. Then, compute the least common multiple
+    # of those steps, which gives us the total number of steps after which we
+    # arrive at all Z nodes at the same time.
     required_steps_for_each_a_node = []
-    a_nodes = {node for node in network if node.endswith("A")}
+    a_nodes = filter(lambda node: node.endswith("A"), network)
     for node in a_nodes:
-        i = 0
         steps = 0
         while not node.endswith("Z"):
-            node = network[node][instructions[i]]
+            node = network[node][instructions[steps % len(instructions)]]
             steps += 1
-            i = (i + 1) % len(instructions)
         required_steps_for_each_a_node.append(steps)
     return math.lcm(*required_steps_for_each_a_node)
 
