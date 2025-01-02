e4e02610d76ba12437dc5ed045e7465738d2b056
Petr Zemek <s3rvac@petrzemek.net>
2025-01-02 07:20:00 +0100

    aoc-2023-23: Speed up the solution to part 2
    See the comments in the code. By compressing the graph, we can massively speed
    up the computation.

--- 2023/23/aoc23_part2.py
+++ 2023/23/aoc23_part2.py
@@ -2,21 +2,9 @@
 # Advent of Code 2023, day 23, part 2
 #
 
-import dataclasses
+import collections
 import textwrap
 import unittest
-
-
-@dataclasses.dataclass
-class Hike:
-    i: int
-    j: int
-    steps: int
-    visited: set
-
-    def step_to(self, i, j):
-        visited = self.visited | {(self.i, self.j)}
-        return Hike(i, j, self.steps + 1, visited)
 
 
 def read_input():
@@ -29,39 +17,70 @@ def parse_input(input):
 
 
 def get_longest_hike_length(map):
-    # We perform a depth-first search (but a breadth-first search would work as
-    # well) and store the length of each hike. Then, we return the length of
-    # the longest hike.
-    longest_hike_length = 0
+    # We find the longest hike in two steps:
+    #
+    # 1) First, we construct a graph out of the grid in a way that minimizes
+    #    the number of nodes by compressing "long halls" that do not have any
+    #    turns. This has to be done in order to make the second step
+    #    computationally feasible.
+    # 2) Second, we use the graph to find the longest hike by checking all the
+    #    hikes in it.
 
     # The end is always in the last row and second-to-last column.
-    END_I_J = len(map) - 1, len(map[-1]) - 2
+    END = len(map) - 1, len(map[-1]) - 2
 
-    # The start is always in the first row and second column.
-    hikes_to_check = [Hike(i=0, j=1, steps=0, visited=set())]
+    # Graph format: (i, j): {(neighbor i, neighbor j, distance), ...}
+    graph = collections.defaultdict(set)
 
-    while hikes_to_check:
-        hike = hikes_to_check.pop()
-        for ix, jx in [(0, +1), (1, 0), (-1, 0), (0, -1)]:
-            i = hike.i + ix
-            j = hike.j + jx
+    # Step 1): Use depth-first search to construct the graph.
+    # Stack format: (i, j, from_i, from_j, orig_i, orig_j, distance)
+    s = [(1, 1, 0, 1, 0, 1, 1)]
+    visited = set()
+    while s:
+        i, j, from_i, from_j, orig_i, orig_j, distance = s.pop()
+
+        if (i, j) in visited or (i, j) == END:
+            graph[(orig_i, orig_j)].add((i, j, distance))
+            continue
+
+        neighbors = []
+        for ni, nj in [(i, j + 1), (i + 1, j), (i - 1, j), (i, j - 1)]:
             if (
-                i < 0
-                or i >= len(map)
-                or j < 0
-                or j >= len(map[0])
-                or map[i][j] == "#"
-                or (i, j) in hike.visited
+                (ni, nj) != (from_i, from_j)
+                and 0 <= ni < len(map)
+                and 0 <= nj < len(map[i])
+                and map[ni][nj] != "#"
             ):
+                neighbors.append((ni, nj))
+        if len(neighbors) == 1:
+            # A "long hall".
+            s.append((*neighbors[0], i, j, orig_i, orig_j, distance + 1))
+        elif len(neighbors) > 1:
+            # Multiple neighbors (i.e. a "hub" node).
+            visited.add((i, j))
+            graph[(orig_i, orig_j)].add((i, j, distance))
+            graph[(i, j)].add((orig_i, orig_j, distance))
+            for ni, nj in neighbors:
+                s.append((ni, nj, i, j, i, j, 1))
+
+    # Step 2): Check all possible hikes to find the longest one.
+    hike_lengths = set()
+
+    # Format: (i, j, steps, visited nodes)
+    hikes_to_check = [(0, 1, 0, set())]
+    while hikes_to_check:
+        i, j, steps, visited = hikes_to_check.pop()
+        for new_i, new_j, distance in graph[(i, j)]:
+            if (new_i, new_j) == END:
+                hike_lengths.add(steps + distance)
                 continue
 
-            if (i, j) == END_I_J:
-                longest_hike_length = max(hike.steps + 1, longest_hike_length)
-                continue
+            if (new_i, new_j) not in visited:
+                hikes_to_check.append(
+                    (new_i, new_j, steps + distance, visited | {(new_i, new_j)})
+                )
 
-            hikes_to_check.append(hike.step_to(i, j))
-
-    return longest_hike_length
+    return max(hike_lengths)
 
 
 def run_program(input):
@@ -75,48 +94,6 @@ if __name__ == "__main__":
 
 
 class Tests(unittest.TestCase):
-    def test_program_returns_correct_result_for_my_input1(self):
-        input = textwrap.dedent(
-            """
-            #.#
-            #.#
-            #.#
-            """
-        )
-
-        result = run_program(input)
-
-        self.assertEqual(result, 2)
-
-    def test_program_returns_correct_result_for_my_input2(self):
-        input = textwrap.dedent(
-            """
-            #.###
-            #...#
-            #...#
-            ###.#
-            """
-        )
-
-        result = run_program(input)
-
-        self.assertEqual(result, 7)
-
-    def test_program_returns_correct_result_for_my_input3(self):
-        input = textwrap.dedent(
-            """
-            #.###
-            #.>.#
-            #.>.#
-            #.>.#
-            ###.#
-            """
-        )
-
-        result = run_program(input)
-
-        self.assertEqual(result, 10)
-
     def test_program_returns_correct_result_for_example_input(self):
         input = textwrap.dedent(
             """
